b9b0dc634a063c68d2d251575d6c270750331cc5
FarshadJahanfard <116713713+FarshadJahanfard@users.noreply.github.com>
2025-04-09 22:18:18 +0100

    127001

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,24 +1,15 @@
 import Dashboard from "./dashboard";
-import { headers } from 'next/headers';
 
 const getBookings = async () => {
-  const headersList = headers();
-  const host = headersList.get('host');
-  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
-  const baseUrl = `${protocol}://${host}`;
-
-  const res = await fetch(`${baseUrl}/api/bookings`, {
+  const res = await fetch(`http://127.0.0.1:3000/api/bookings`, {
     method: 'GET',
-    cache: 'no-store',
+    cache: 'no-store'
   });
-
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
-
   return res.json();
-};
-
+}
 export default async function Page() {
     const bookings = await getBookings();
     console.log(bookings);
