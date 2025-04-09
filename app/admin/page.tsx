7974d436dc67f09699206ae174ad89d9f2c00bb8
FarshadJahanfard <116713713+FarshadJahanfard@users.noreply.github.com>
2025-04-09 22:23:59 +0100

    next url

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,20 +1,25 @@
 import Dashboard from "./dashboard";
 
 const getBookings = async () => {
-  const res = await fetch(`http://127.0.0.1:3000/api/bookings`, {
-    method: 'GET',
-    cache: 'no-store'
-  });
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/bookings`, {
+      method: 'GET',
+      cache: 'no-store'
+    });
+    if (!res.ok) {
+      throw new Error(`Fetch failed with status ${res.status}`);
+    }
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching bookings:", error);
+    throw new Error("Failed to fetch data");
   }
-  return res.json();
-}
-export default async function Page() {
-    const bookings = await getBookings();
-    console.log(bookings);
-    return (
-    <Dashboard mockBookings={bookings.reservations}/>
-  )
-}
+};
 
+export default async function Page() {
+  const bookings = await getBookings();
+  console.log(bookings);
+  return (
+    <Dashboard mockBookings={bookings.reservations} />
+  );
+}
